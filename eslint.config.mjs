384fef60560558ce0bfbc1c05e3879d793e90891
Wilds Rincon <wildsrincon.developer@gmail.com>
2025-01-08 22:59:59 -0500

    feat: adding tickets page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,35 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    extends: ["eslint:recommended"], // Base recommended rules
+  },
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:react/recommended"
+  ),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "react/react-in-jsx-scope": 0,
+    },
+  },
 ];
 
 export default eslintConfig;
---
+++ src/app/tickets/page.tsx
@@ -0,0 +1,5 @@
+const TicketsPage = () => {
+  return <h2 className="text-lg">Tickets Page</h2>;
+};
+
+export default TicketsPage;
